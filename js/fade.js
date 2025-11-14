727997e2153969c28187ec3260fd7db533bee065
Scandianx <scandianidev@gmail.com>
2025-11-14 17:26:52 -0300

    add: efeitos 99% finalizados

--- js/fade.js
+++ js/fade.js
@@ -1,10 +1,11 @@
-const items=document.querySelectorAll(".fade-in")
+const animated=document.querySelectorAll(".zoom-in,.drop-in,.slide-left,.fade-in,.blur-in,.light-in,.skew-in,.elastic-in")
 const observer=new IntersectionObserver(e=>{
   e.forEach(i=>{
     if(i.isIntersecting){
       i.target.classList.add("show")
+      observer.unobserve(i.target)
     }
   })
 },{threshold:.2})
 
-items.forEach(i=>observer.observe(i))
+animated.forEach(i=>observer.observe(i))
